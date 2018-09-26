b68a361e31ede9765fbbb8cc65bc0c324d35759f
Arora <mohit.arora@igt.com>
2018-09-26 18:29:28 +0530

    Kmeans

--- Kmeans.py
+++ Kmeans.py
@@ -1,26 +1,41 @@
+from copy import deepcopy
 import numpy as np
-import copy as cp
-def distance(a,b,x) :
-	return dist = numpy.linalg.norm(a-b,axis = x)
+import pandas as pd
+from matplotlib import pyplot as plt
 
-A=['1','2','4','5','1','3','7']
+# Importing the dataset
+data = pd.read_csv('Data.csv')
+print("Input Data and Shape")
+print(data.shape)
+data.head()
 
-k=[2,7,5]
-error = 1
-while error ! = 0
-	for i in range(len(A)):
-		distances = distance(A[i],k)
-		cluster[i] = distance.index(min(distance))
-	k_old = cp.deepcopy(k)
+f1 = data['Value'].values
+X = np.array(f1)
 
-	for i in range(len(k)):
-		dp = np.array(X[j]) for j in range(len(A) if clusters[j] == i)
-		k[i] = np.mean(dp)
-	
-	error= dist(k, k_old, None)
+def dist(a, b, ax = 0):
+    return np.linalg.norm(a - b, axis=ax)
 
-print (A)
-print (clusters)
+k = 3
+C_x = np.random.randint(0, np.max(X), size=k)
 
+C = np.array(C_x,dtype=np.float32)
+print(C)
 
+C_old = np.zeros(C.shape)
+clusters = np.zeros(len(X))
+error = dist(C, C_old, None)
 
+while error != 0:
+    for i in range(len(X)):
+        distances = dist(X[i],C,None)
+        cluster = np.argmin(distances)
+        clusters[i] = cluster
+    C_old = deepcopy(C)
+    for i in range(k):
+        points = [X[j] for j in range(len(X)) if clusters[j] == i]
+        S = sum(points)
+        C[i] = S/len(points)    
+    error = dist(C, C_old, None)
+
+print(clusters)
+print(C)
