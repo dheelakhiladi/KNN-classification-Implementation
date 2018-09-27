0a06d5c2d89da885db211efe7c84952fe5e0cdd4
Arora <mohit.arora@igt.com>
2018-09-27 13:01:07 +0530

    K-means with one feature done

--- Kmeans.py
+++ Kmeans.py
@@ -1,9 +1,7 @@
 from copy import deepcopy
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 
-# Importing the dataset
 data = pd.read_csv('Data.csv')
 print("Input Data and Shape")
 print(data.shape)
@@ -11,6 +9,8 @@ data.head()
 
 f1 = data['Value'].values
 X = np.array(f1)
+
+print(X)
 
 def dist(a, b, ax = 0):
     return np.linalg.norm(a - b, axis=ax)
@@ -24,18 +24,47 @@ print(C)
 C_old = np.zeros(C.shape)
 clusters = np.zeros(len(X))
 error = dist(C, C_old, None)
-
+K = np.zeros(len(C))
+index_mi = 0
+Sum = 0
+algo_it = 0
+C_old = np.zeros(len(C))
+#print error
 while error != 0:
-    for i in range(len(X)):
-        distances = dist(X[i],C,None)
-        cluster = np.argmin(distances)
-        clusters[i] = cluster
-    C_old = deepcopy(C)
-    for i in range(k):
-        points = [X[j] for j in range(len(X)) if clusters[j] == i]
-        S = sum(points)
-        C[i] = S/len(points)    
-    error = dist(C, C_old, None)
+	for i in range(len(C)):
+			C_old[i] = C[i]
+			pass
+	for i in range(len(X)):
+		for j in range(len(C)):
+			K[j] = dist(X[i],C[j],None)
+			#print(K)
+			pass
+			mi = min(K)
+			for it in range(len(K)):
+				if K[it] == mi:
+					index_mi = it
+					clusters[i] = C[index_mi]
+		pass
+	pass
+	
+	it_c = 0
+	for j in range(len(C)):
+		Sum = 0
+		count = 0
+		for i in range(len(X)):
+			if clusters[i] == C[j]:
+				Sum += X[i]
+				count = count+1
+				pass
+		C[j] = Sum/count
+		it = it+1
+		pass
+	error = dist(C,C_old,None)
+	algo_it = algo_it+1
+pass
 
-print(clusters)
 print(C)
+print(X)
+print(clusters)
+print(algo_it)
+
