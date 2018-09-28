236160dea3759c01e9057a919534a3bf4ddaaa34
Arora <mohit.arora@igt.com>
2018-09-28 15:51:13 +0530

    Kmeans with refactored data added

--- Kmeans(n).py
+++ Kmeans(n).py
@@ -10,13 +10,17 @@ print('Input Data and Shape')
 print(data.shape)
 data.head()
 print (data.columns)
+data = data = data.loc[((data[data.columns[0]] > 0) & (data[data.columns[0]] < 1000)) & ((data[data.columns[1]] > 0)&(data[data.columns[1]] < 900))]
+print(data)
 
 feature1 = data[data.columns[0]].values
 feature2 = data[data.columns[1]].values
 feature1 = (feature1 - np.mean(feature1))/(max(feature1) - min(feature1))
 feature2 = (feature2 - np.mean(feature2))/(max(feature2) - min(feature2))
+
 print('feature1 is Volume')
 print('feature2 is Average Average price')
+
 X = np.array(list(zip(feature1,feature2)))
 Y = pd.DataFrame()
 plt.scatter(feature1, feature2, c='red', s=7 )
@@ -28,14 +32,3 @@ labels = kmeans.predict(X)
 centroids = kmeans.cluster_centers_
 plt.scatter(feature1, feature2, c=labels, cmap = 'viridis')
 plt.show()
-
-#j=0
-
-#for i in range(len(X)):
-#	if kmeans.labels_[i] == 2:
-#		Y[j] = X[i]
-#		j=j+1		
-#		pass
-#print(Y.shape)
-#plt.scatter(Y[0,:],Y[1,:],c = labels, cmap = 'viridis')
-# plt.show()
